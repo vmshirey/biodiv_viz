64d9c9b1dfa60f56afde04d95509399c622ab22e
Vaughn Shirey <vms44@drexel.edu>
2017-05-11 11:07:45 -0400

    - added Papilio glaucus occurrence records
    added additional data for testing.

--- Timeline/Timeline/server.R
+++ Timeline/Timeline/server.R
@@ -11,8 +11,8 @@ library(dplyr)
 library(shiny)
 library(googleVis)
 
-# load data & aggregate for counts
-O_fasciatus_dat <- read.csv("occurrence.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+# load data & aggregate for counts (occurence_lmb.csv has O. fasciatus data)
+O_fasciatus_dat <- read.csv("occurrence_lmb.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
 
 dat <- aggregate(O_fasciatus_dat$dwc.individualCount~O_fasciatus_dat$idigbio.eventDate, FUN=length, data=O_fasciatus_dat)
 dat <- na.omit(dat)
@@ -26,7 +26,11 @@ shinyServer(function(input, output) {
   output$Plot <- renderGvis({
     
     # generate GVis Calendar for occurrence records
-    Cal <- gvisCalendar(dat, datevar="O_fasciatus_dat$idigbio.eventDate", numvar="O_fasciatus_dat$dwc.individualCount", options=list(width=1000, height=24000))
+    Cal <- gvisCalendar(dat, datevar="O_fasciatus_dat$idigbio.eventDate", numvar="O_fasciatus_dat$dwc.individualCount", options=list(width=1000, height=24000,
+                                                                                                                                     calendar="{yearLabel: { fontName: 'Times-Roman',
+                                                                                                                                     fontSize: 32, color: '#4682B4', bold: true},
+                                                                                                                                     cellSize: 10, cellColor: { stroke: 'grey', strokeOpacity: 0.2 },
+                                                                                                                                     focusedCellColor: {stroke: 'red'}}"))
     
   })
   
