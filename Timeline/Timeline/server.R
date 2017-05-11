0d2b1b58d2249784f02d7ed2507b29cb95953d15
Vaughn Shirey <vms44@drexel.edu>
2017-05-11 10:55:59 -0400

    - added data & GVis Calendar for testing
    Added a datafile containing iDigBio occurrence records for the Large
    Milkweed Bug and set up basic GVis calendar without interactivity for
    viewing the distribution of records.

--- Timeline/Timeline/server.R
+++ Timeline/Timeline/server.R
@@ -7,19 +7,26 @@
 #    http://shiny.rstudio.com/
 #
 
+library(dplyr)
 library(shiny)
+library(googleVis)
+
+# load data & aggregate for counts
+O_fasciatus_dat <- read.csv("occurrence.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+
+dat <- aggregate(O_fasciatus_dat$dwc.individualCount~O_fasciatus_dat$idigbio.eventDate, FUN=length, data=O_fasciatus_dat)
+dat <- na.omit(dat)
+dat$`O_fasciatus_dat$idigbio.eventDate`<- strptime(x=as.character(dat$`O_fasciatus_dat$idigbio.eventDate`),
+                                                   format = "%Y-%m-%dT%H:%M")
+dat <- dat[!is.na(dat$`O_fasciatus_dat$idigbio.eventDate`),]
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$Plot <- renderGvis({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # generate GVis Calendar for occurrence records
+    Cal <- gvisCalendar(dat, datevar="O_fasciatus_dat$idigbio.eventDate", numvar="O_fasciatus_dat$dwc.individualCount", options=list(width=1000, height=24000))
     
   })
   
